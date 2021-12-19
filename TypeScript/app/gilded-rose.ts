48005536b6ca9f5765d4bad22294513e5ac6df7d
Polina Demochkina <lollipoli@live.com>
2021-12-19 13:09:55 -0500

    Re-done TypeScript homework

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,70 +1,150 @@
+export class GildedRose {
+    items: Array<Product>
+
+    constructor(items = [] as Array<Product>) {
+        this.items = items
+    }
+
+    public static getProductType (name: string) {
+        let loweredName = name.toLowerCase();
+    
+        if (loweredName.includes('aged brie')) {
+            return ProductTypes.AgedBrie;
+        } else if (loweredName.includes('sulfuras')) {
+            return ProductTypes.Sulfuras;
+        } else if (loweredName.includes('backstage pass')) {
+            return ProductTypes.BackstagePass;
+        } else if (loweredName.includes('conjured')) {
+            return ProductTypes.Conjured;
+        } else {
+            return ProductTypes.Normal;
+        }
+    }
+    
+    public static instantiateItem = (name: string, quality: number, sellIn: number): Product => {
+        let type = this.getProductType(name);
+    
+        switch (type) {
+    
+            case ProductTypes.AgedBrie:
+                return new AgedBrie(name, quality, sellIn);
+            case ProductTypes.Sulfuras:
+                return new Sulfuras(name, quality, sellIn);
+            case ProductTypes.BackstagePass:
+                return new BackstagePass(name, quality, sellIn);
+            case ProductTypes.Conjured:
+                return new Conjured(name, quality, sellIn);
+            default:
+                return new Normal(name, quality, sellIn);
+        }
+    }
+
+    updateQuality() {
+        this.items.forEach((item) => item.tickForward())
+    }
+}
+
+export enum ProductTypes {
+
+    Normal= 'Normal',
+    AgedBrie= 'AgedBrie',
+    Sulfuras= 'Sulfuras',
+    BackstagePass= 'BackstagePass',
+    Conjured = 'Conjured'
+
+}
+
 export class Item {
     name: string;
-    sellIn: number;
     quality: number;
-
+    sellIn: number;
+	
     constructor(name, sellIn, quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
+	
+    static createItem(name: string, quality: number, sellIn: number) {
+        return GildedRose.instantiateItem(name, quality, sellIn);
+    }
 }
 
-const MAX_QUALITY = 50
+export class Product extends Item {
+	tickForward(): void {
+		this.sellIn = this.tomorrowSellIn();
+		this.quality = Math.max(this.tomorrowQuality(), 0);
+		if (GildedRose.getProductType(this.name) != ProductTypes.Sulfuras)
+			this.quality = Math.min(this.quality, 50);
+	}
 
-export class GildedRose {
-    items: Array<Item>
+	tomorrowSellIn(): number {
+		if (this.sellIn > 0)
+			return this.sellIn - 1;
+		return 0;
+	}
+	
+	tomorrowQuality(): number {
+		return 0;
+	}
+}
 
-    constructor(items = [] as Array<Item>) {
-        this.items = items
-    }
-
-    updateQuality() {
-        for (var i = 0; i < this.items.length; i++) {
-			if (this.items[i].sellIn > 0)
-				this.items[i].sellIn--
-			
-			if (this.items[i].name.includes('Sulfuras')) {
-				this.items[i].sellIn = 0
-				this.items[i].quality = 80
-			}
-			
-			else if (this.items[i].name.includes('Aged Brie') && (this.items[i].quality < MAX_QUALITY)) {
-				this.items[i].quality++
-			}
-			
-			else if (this.items[i].name.includes('Backstage passes')) {
-				if (this.items[i].sellIn == 0) {
-					this.items[i].quality = 0
-				} else if (this.items[i].quality < MAX_QUALITY) {
-					this.items[i].quality++
-					if (this.items[i].sellIn <= 10 && this.items[i].quality < MAX_QUALITY)
-						this.items[i].quality++
-					if (this.items[i].sellIn <= 5 && this.items[i].quality < MAX_QUALITY)
-						this.items[i].quality++
-				}
-			}
-			
-			else if (this.items[i].name.includes('Conjured')) {
-				var minus = 2
-				if (this.items[i].sellIn == 0)
-					minus *= 2
-				if (this.items[i].quality - minus < 0)
-					this.items[i].quality = 0
-				else
-					this.items[i].quality -= minus
-			}
-			
-			else {
-				if (this.items[i].sellIn > 0 && this.items[i].quality > 0)
-					this.items[i].quality--
-				else if (this.items[i].sellIn == 0 && this.items[i].quality - 2 >= 0)
-					this.items[i].quality -= 2
-				else
-					this.items[i].quality = 0
-			}
+export class Normal extends Product{
+	tomorrowQuality(): number {
+        let outPut: number = this.quality - 1;
+        if (this.sellIn == 0) {
+            outPut--;
         }
 
-        return this.items;
+        return outPut;
+    }
+}
+
+export class AgedBrie extends Product {
+    tomorrowQuality(): number {
+        return this.quality + 1;
+    }
+}
+
+export class Sulfuras extends Product {
+    tomorrowQuality(): number {
+        return 80;
+    }
+
+    tomorrowSellIn(): number {
+        return this.sellIn;
+    }
+}
+
+export class BackstagePass extends Product {
+    tomorrowQuality(): number {
+        if (this.sellIn == 0)
+            return 0;
+
+        let output: number = this.quality + 1;
+
+        if (this.sellIn <= 10) {
+            output++;
+        }
+
+        if (this.sellIn <= 5) {
+            output++;
+        }
+
+        return output;
+
+    }
+}
+
+export class Conjured extends Product {	
+    tomorrowQuality(): number {
+        let outPut: number = this.quality - 2;
+
+        if (this.sellIn <= 0) {
+            outPut-= 2;
+        }
+
+        return outPut;
+
     }
 }
